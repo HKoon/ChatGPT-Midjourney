cb2b721a0de807e901c86c8eb8169a3a569d6c02
Hillay Koon <iace@foxmail.com>
2023-08-28 23:10:14 +0900

    修改标题

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT-Midjourney</div>
+        <div className={styles["sidebar-title"]}>HelloGPT-Midjourney</div>
         <div className={styles["sidebar-sub-title"]}>
-          Guan own AI assistant.
+          Guan's AI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
